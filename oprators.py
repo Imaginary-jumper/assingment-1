29beb9aca0bfc61f489cd022f4c5bc02f33aa92c
Imaginary-jumper <ankit2102karmakar@gmail.com>
2025-05-27 22:50:55 +0530

    Add files via upload

---
+++ oprators.py
@@ -0,0 +1,7 @@
+a = int(input("Please enter a number:"))
+b = int(input("Please enter a 2nd number:"))
+
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
